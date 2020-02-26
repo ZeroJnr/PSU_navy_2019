cbb9611c6fd401284d4ef297412301792e6e438b
Liam <liam.esteffe@epitech.eu>
2020-02-26 17:19:21 +0100

    <clean> <project>
    test liam branch to start project in good conditions

--- src/main_functions/navy.c
+++ src/main_functions/navy.c
@@ -15,10 +15,10 @@ void my_free(game_t *game)
 
 int navy(game_t *game, int ac, char **av)
 {
-    /*if (ac == 1)
+    if (ac == 1)
         user1(game);
     else if (ac == 2)
-        user2(game, av[1]);*/
+        user2(game, av[1]);
     if (create_map(game) == 84) {
         my_free(game);
         close(game->map.fd);
